ec0b2411d656255d59810a6a13bb255b25c19c3e
Nadder Ragi Michel Selim <93446325+folanelfolany@users.noreply.github.com>
2024-07-06 19:14:07 +0300

    Version3
    Adjusted Index Values

--- storage-microservices/src/app.controller.spec.ts
+++ storage-microservices/src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello Worldddd!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
